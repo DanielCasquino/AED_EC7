abff20aedebe88b710cef13561439778c8e8bc72
Daniel Casquino <daniel.casquino@utec.edu.pe>
2024-01-30 11:11:12 -0500

    1 done, 3 almost done

--- MedianFinder.cpp
+++ MedianFinder.cpp
@@ -1,17 +1,53 @@
-class MedianFinder {
+#include <queue>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class MedianFinder
+{
+    priority_queue<int, vector<int>> maxHeap;
+    priority_queue<int, vector<int>, std::greater<>> minHeap;
+
 public:
-    MedianFinder() {
-        
+    MedianFinder()
+    {
     }
-    
-    void addNum(int num) {
-        
+
+    void addNum(int num)
+    {
+        if (maxHeap.empty() || maxHeap.size() < minHeap.size())
+        {
+            maxHeap.push(num);
+        }
+        else
+        {
+            minHeap.push(num);
+        }
     }
-    
-    double findMedian() {
-        
+
+    double findMedian()
+    {
+        if ((maxHeap.size() + minHeap.size()) % 2 == 0)
+        {
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        }
+        return maxHeap.top();
     }
 };
+
+int main()
+{
+    MedianFinder *obj = new MedianFinder();
+    obj->addNum(2);
+    obj->addNum(3);
+    obj->addNum(5);
+    obj->addNum(6);
+    double param_2 = obj->findMedian();
+    cout << param_2;
+    return 0;
+}
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
